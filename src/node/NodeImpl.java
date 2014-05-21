7e81483c3d66431c4eb9c02d194f876b156b4f65
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-20 23:58:10 -0500

    Implementation of Node in the network

---
+++ src/node/NodeImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package node;
+
+/**
+ * @author Dany
+ *
+ */
+public class NodeImpl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
