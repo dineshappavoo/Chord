6871d5d532fd29957ed5ddaad1ef73783824bc13
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-21 23:36:06 -0500

    Added method definition for Node interface

--- src/node/NodeImpl.java
+++ src/node/NodeImpl.java
@@ -3,18 +3,68 @@
  */
 package node;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
 /**
  * @author Dany
+ * @param <E>
  *
  */
-public class NodeImpl {
+public class NodeImpl<E> extends UnicastRemoteObject implements Node<E>{
 
-	/**
-	 * @param args
-	 */
+
+	protected NodeImpl() throws RemoteException {
+		super();
+	}
+
 	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public String getKey() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node<E> getSuccessor() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSuccessor() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
+
+	@Override
+	public void setPredecessor() throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Node<E> getPredecessor() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node<E	> lookup(String key) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateFingers(List<Node<E>> nodes) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+
 
 }
