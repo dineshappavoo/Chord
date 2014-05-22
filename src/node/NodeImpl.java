0d36dfc78407ed7f4c5f86f09a85b0344580b23c
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-21 23:45:28 -0500

    Updated the Node impl for setters and method definitions

--- src/node/NodeImpl.java
+++ src/node/NodeImpl.java
@@ -5,63 +5,75 @@ package node;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * @author Dany
+ * @author Dinesh Appavoo
  * @param <E>
  *
  */
 public class NodeImpl<E> extends UnicastRemoteObject implements Node<E>{
 
+	private String name;
+	
+	private String key;
+	
+	private Node<E> predecessor;
+	
+	private Node<E> successor;
+	
+	private Map<String, Node<E>> fingers=new LinkedHashMap<String, Node<E>>();
 
+	/**
+	 * 
+	 * @throws RemoteException
+	 */
 	protected NodeImpl() throws RemoteException {
 		super();
-	}
-
-	public static void main(String[] args) {
-
-	}
+	}	
 
 	@Override
 	public String getKey() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return key;
 	}
 
 	@Override
 	public Node<E> getSuccessor() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return successor;
 	}
 
 	@Override
-	public void setSuccessor() throws RemoteException {
-		// TODO Auto-generated method stub
-
+	public void setSuccessor(Node<E> successor) throws RemoteException {
+		this.successor=successor;
+		
 	}
 
 	@Override
-	public void setPredecessor() throws RemoteException {
-		// TODO Auto-generated method stub
-
+	public void setPredecessor(Node<E> predecessor) throws RemoteException {
+		this.predecessor=predecessor;
+		
 	}
-
+	
 	@Override
 	public Node<E> getPredecessor() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return predecessor;
 	}
 
 	@Override
-	public Node<E	> lookup(String key) throws RemoteException {
-		// TODO Auto-generated method stub
+	public Node<E> lookup(String key) throws RemoteException {
 		return null;
 	}
 
 	@Override
 	public void updateFingers(List<Node<E>> nodes) throws RemoteException {
-		// TODO Auto-generated method stub
+
+	}
+
+
+	public static void main(String[] args) {
 
 	}
 
